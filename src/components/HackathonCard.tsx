e91117b13c2f9c5f882f8d7dcea592d8176545b1
Ricardo <dones.bugan@gmail.com>
2025-11-25 08:47:40 -0300

    Updated HackathonCard.tsx

--- src/components/HackathonCard.tsx
+++ src/components/HackathonCard.tsx
@@ -47,7 +47,7 @@ export const HackathonCard: React.FC<HackathonCardProps> = ({
       console.log('Hackathon empresa_id:', hackathon);
       console.log('Deleting hackathon:', hackathon.id);
       console.log(new Date().toISOString());
-      console.log(Date.now().toISOString());
+      console.log(Date.now().toISOString);
       const { data, error } = await supabase
         .from('hackathons')
         .update({ deleted_at: new Date().toISOString()})
